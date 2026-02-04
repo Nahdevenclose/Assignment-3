1b44e8a228e895a6b854398f2d07786dae84d167
Nahdevenclose <nbenson@aggies.ncat.edu>
2026-02-04 15:04:30 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,3 +3,8 @@ print("Student email: jsmith@ncat.edu")
 print("Hometown: Charlotte, NC")
 print("Graduation semester: Spring 2028")
 print("Major: Computer Science")
+
+current_course_list = ["COMP 163", "MATH 150", "ENG 101", "HIS 105"]
+completed_course_list = ["Biology", "Chemistry", "Calculus", "Spanish II", "World History"]
+credit_hour_list = [3, 3, 3, 3]
+gpa_history = [3.2, 3.6, 3.4, 3.7]
