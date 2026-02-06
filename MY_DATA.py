3ce440f8f323c2d9ad0c73be07d77933f7f716a8
Nahdevenclose <nbenson@aggies.ncat.edu>
2026-02-06 02:23:23 -0500

    Add files via upload
    This is section 1

---
+++ MY_DATA.py
@@ -0,0 +1,47 @@
+full_name = "Nahdia Benson"
+student_email = "nbenson@aggies.ncat.edu"
+hometown = "Norcross, Georgia"
+graduation = "May 2026"
+major = "Mechanical Engineering"
+
+print("Full name:", full_name)
+print("Student email:", student_email)
+print("Hometown:", hometown)
+print("Graduation semester:", graduation)
+print("Major:", major)
+
+current_course_list = ["ART 100", "COMP 163", "MEEN 424", "MEEN 425", "MEEN 481", "MEEN 492"]
+completed_course_list = ["Circuits", "Linear Algebra", "Thermal-Fluid Lab", "Systems Dynamics", "Turbomachineary", "Senior Project"]
+
+print("Current courses:", current_course_list)
+print("Completed courses:", completed_course_list)
+
+current_skills = {"C++", "CAD", "JavaScript", "Problem Solving", "Time Management"}
+skills_to_learn = {"Data Structures", "Git", "Web design", "Machine Learning"}
+career_interests = {"Robotics Engineering", "Manufacturing Engineering", "Mechatronics", "Research and Development"}
+hobbies = {"Hanging with dog and cat", "Robotics", "Reading", "Music"}
+
+print("Current skills:", current_skills)
+print("Skills to learn:", skills_to_learn)
+print("Career interests:", career_interests)
+print("Hobbies:", hobbies)
+
+food_budget = 250
+rent_budget = 450
+pet_budget = 100
+subscriptions_budget = 50
+
+print("Monthly budget: $", food_budget + rent_budget + pet_budget + subscriptions_budget)
+
+study_hours = {"ART 100": 1, "COMP 163": 5, "MEEN 424": 4, "MEEN 425": 3, "MEEN 481": 5, "MEEN 492": 1}
+
+print("Weekly study hours:", study_hours["ART 100"]+ study_hours["COMP 163"] + study_hours["MEEN 424"] + study_hours["MEEN 425"] + study_hours["MEEN 481"] + study_hours["MEEN 492"])
+
+insta_info = ("Instagram", "@onyx_the_noodle_doodle", 113)
+tiktok_info = ("TikTok", "@nahdevenclose", 51)
+
+print("Instagram:", insta_info)
+print("TikTok:", tiktok_info)
+
+
+
